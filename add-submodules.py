4761ab4321bfd53c7c109e0580117c1e9244e540
Dominik Jurkovic <d.jurkovic@reply.de>
2022-08-18 13:09:01 +0200

    Add jenkins plugins as submodules

---
+++ add-submodules.py
@@ -0,0 +1,29 @@
+import subprocess
+import requests
+
+github_url = 'https://github.com/jenkinsci/'
+
+def addGitSubmodule(plugin_name):
+    bashCommand = "git submodule add " + github_url + plugin_name
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    if (str(error) == 'None'):
+        print(error)
+
+with open('plugins.txt') as file:
+
+    for line in file:
+
+        plugin_name = line.rstrip().split(":", 1)[0]
+
+        if plugin_name.endswith('plugin'):
+            response = requests.get(github_url + plugin_name)
+            if response.status_code == 200:
+                addGitSubmodule(plugin_name)
+        else:
+            plugin_name += '-plugin'
+            response = requests.get(github_url + plugin_name)
+            if response.status_code == 200:
+                addGitSubmodule(plugin_name)
+
+        print(plugin_name)
